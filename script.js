b3dc3bd7533f7e3fab3e1fe02f6fe9b5a01dc108
CosminIlincariu <89630793+CosminIlincariu@users.noreply.github.com>
2021-09-09 14:48:34 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,49 +4,64 @@ const guess = document.querySelector(".guess");
 const checkBtn = document.querySelector(".check");
 const message = document.querySelector(".message");
 const bodyEl = document.getElementById("body");
-const score = document.querySelector(".score");
 const highscore = document.querySelector(".highscore");
 const number = document.querySelector(".number");
 const againBtn = document.querySelector(".again");
 
 let randomValue = 0;
+let score = 20;
 
 const luckyNumber = () => {
   randomValue = Math.trunc(Math.random() * 20) + 1;
   return randomValue;
 };
 
-luckyNumber();
-console.log(randomValue);
 const writeMessage = (display) => {
   message.textContent = display;
 };
 
-checkBtn.addEventListener("click", () => {
-  let inputNumber = Number(guess.value);
+luckyNumber();
+console.log(randomValue);
 
-  if (!inputNumber) {
-    writeMessage(`⛔ Not a number!`);
+checkBtn.addEventListener("click", () => {
+  const inputNumber = Number(document.querySelector(".guess").value);
+
+  if (!inputNumber || inputNumber < 0 || inputNumber > 20) {
+    writeMessage(`⛔ Choose a number between 1 and 20`);
     return;
   }
 
   if (inputNumber === randomValue) {
-    writeMessage(`You found it!`);
+    writeMessage(`🎉 You found it!`);
     bodyEl.style.backgroundColor = "#60b347";
     number.textContent = randomValue;
-  } else {
-    writeMessage(`Try again!`);
-    Number(score.textContent--);
-    return;
+    guess.disabled = true;
+    checkBtn.disabled = true;
+    highscore.textContent = score;
+  } else if (inputNumber > randomValue) {
+    writeMessage(`😢 Too high`);
+    score--;
+    document.querySelector(".score").textContent = score;
+  } else if (inputNumber < randomValue) {
+    writeMessage(`😢 Too low`);
+    score--;
+    document.querySelector(".score").textContent = score;
   }
 
-  if (score.textContent > highscore.textContent) {
-    highscore.textContent = score.textContent;
+  if (score === 0) {
+    writeMessage("You lost!");
+    checkBtn.disabled = true;
+    guess.disabled = true;
+    bodyEl.style.backgroundColor = "#8b0000";
+    return;
   }
 });
 
 againBtn.addEventListener("click", () => {
-  score.textContent = 20;
+  guess.disabled = false;
+  checkBtn.disabled = false;
+  score = 20;
+  document.querySelector(".score").textContent = score;
   bodyEl.style.backgroundColor = "#222";
   writeMessage(`Start guessing...`);
   number.textContent = "?";
